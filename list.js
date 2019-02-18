adadbdf8251f6494ed7035f3c24268cc622159b7
jainsimran <itsimmi5@gmail.com>
2019-02-17 20:11:46 -0500

    function working but its not proper (selecting every text)

--- list.js
+++ list.js
@@ -1,22 +1,39 @@
-// function addItem() {
-//   let item = document.getElementById('inputBox');
-//   let submitBtn = document.getElementById('addBtn');
-//   let list = document.getElementById('list');
-//
-//   submitBtn.addEventListener(click, addItem);
-//
-//   if(item.value == ""){
-//     alert("Enter item");
-//   }
-//   else {
-//
-//   }
-// }
 
-function addItem(){
-  let element = document.getElementById(`todoInput`).value;
-  let text = document.createTextNode(element);
-  let newElement = document.createElement("li");
-  newElement.appendChild(text);
-  document.getElementById("todoList").appendChild(newElement);
+let ul = document.getElementById('list');
+let submitBtn = document.getElementById('submit');
+
+// submit button calling addItem function
+submitBtn.onclick = () => {
+  addItem(ul);
+};
+
+// function to add item in list
+function addItem(newItem){
+  let input  = document.getElementById(`todoInput`).value;
+  let text = document.createTextNode(input + ' ');
+  let li = document.createElement("li");
+  let doneBtn = document.createElement('button');
+  document.getElementById('todoInput').value= '';
+
+// show alert if input is empty
+  if(input.length === 0) {
+    alert("input box is empty..");
+    return false;
+  }
+
+  doneBtn.className = 'done';
+  doneBtn.innerHTML = "DONE";
+  doneBtn.setAttribute("onclick", "done(this);");
+
+
+  li.appendChild(text);
+  li.appendChild(doneBtn);
+  document.getElementById("todoList").appendChild(text);
+  document.getElementById("todoList").appendChild(doneBtn);
+}
+
+// to check the item
+function done(item) {
+  let doneItem = document.getElementById('todoList');
+  doneItem.style.textDecoration = "line-through";
 }
